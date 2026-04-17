a0d354b3638746c944655afa011fce3a217dcd90
Jinnn2 <24371426@buaa.edu.cn>
2026-04-17 12:09:17 +0800

    add: ltx2 fixed

--- scripts/ltx2/run_ltx2_t2av_compare_upsample.sh
+++ scripts/ltx2/run_ltx2_t2av_compare_upsample.sh
@@ -2,7 +2,7 @@
 
 # set path and first
 lightx2v_path=/data/yongyang/Jin/LightX2V
-model_path=Lightricks/LTX-2
+model_path=/data/yongyang/Jin/Lightricks/LTX-2
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -13,7 +13,7 @@ python -m lightx2v.infer \
 --model_cls ltx2 \
 --task t2av \
 --model_path $model_path \
---config_json ${lightx2v_path}/configs/ltx2/ltx2_upsample.json \
+--config_json ${lightx2v_path}/configs/ltx2/ltx2_compare_upsample.json \
 --prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage." \
 --negative_prompt "camera shake, overexposed, blurry details, subtitles, low quality, worst quality, jpeg artifacts, deformed hands, deformed face, extra fingers, messy background" \
 --save_result_path ${lightx2v_path}/save_results/output_lightx2v_ltx2_t2av_compare_upsample.mp4
